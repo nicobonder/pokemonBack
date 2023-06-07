bd4053e4139c948d8a0e16630d85e615a04d55ff
nicobonder <nicobonder@hotmail.com>
2023-06-07 11:09:10 -0300

    correcting error in app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ server.use((req, res, next) => {
 res.header('Access-Control-Allow-Origin', 'pokemonfront.up.railway.app');
 
 //res.header('Access-Control-Allow-Origin', 'https://pokemonfront-17cp.onrender.com'); // update to match the domain you will make the request from
-res.header('Access-Control-Allow-Origin', 'pokemon.up.railway.app'); // update to match the domain you will make the request from
+// res.header('Access-Control-Allow-Origin', 'pokemon.up.railway.app'); // update to match the domain you will make the request from CREO QUE ESTA LINEA ESTA MAL
 res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
