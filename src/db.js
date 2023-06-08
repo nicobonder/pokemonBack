4b7e20875644ee2bfda5d4a2e758276f7f20e1c1
nicobonder <nicobonder@hotmail.com>
2023-06-08 11:36:54 -0300

    Trying new connection with DB in db.js

--- src/db.js
+++ src/db.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, { //la db es pokemon en localhost
-//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon_3y02`, {
-  logging: false, // set to console.log to see the raw SQL queries
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, { //la db es pokemon en localhost
+
+const sequelize = new Sequelize(DATABASE_URL, {
+logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
