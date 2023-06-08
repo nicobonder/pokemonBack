099350709e7270cf8bf9c490d4d23030fa043b97
nicobonder <nicobonder@hotmail.com>
2023-06-08 11:40:25 -0300

    Trying new connection with DB in db.js

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const {
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, { //la db es pokemon en localhost
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
 logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
